3ae96c6b25684cd10f9e36d2bd5863b447a62cf1
anas790 <anas@medblocks.org>
2023-10-18 23:48:24 +0530

    # removing common dependency project

--- common_dependency/src/main/java/com/medblocks/common_dependency/CommonDependencyApplication.java
+++
@@ -1,9 +0,0 @@
-package com.medblocks.common_dependency;
-
-public class CommonDependencyApplication {
-
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-
-}
--- common_dependency/src/main/java/com/medblocks/common_dependency/WebSocketSender.java
+++
@@ -1,5 +0,0 @@
-package com.medblocks.common_dependency;
-
-public interface WebSocketSender {
-	void sendMessage(String userID, String message);
-}
