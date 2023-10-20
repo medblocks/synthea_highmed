45497154081e0599e456bdc03ff778b10c1b6d82
anas790 <anas@medblocks.org>
2023-10-21 01:16:13 +0530

    # websocket fix 2

--- highmed/src/main/java/com/medblocks/highmed/config/WebSocketConfig.java
+++ highmed/src/main/java/com/medblocks/highmed/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.medblocks.highmed.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,6 +12,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/ws")
+                        .allowCredentials(true)
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+
+            }
+        };
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
